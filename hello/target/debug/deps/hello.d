8662fdd3037412af3a9ffdac7d65df79e7a7d759
alvinchristian <alvch6@gmail.com>
2026-04-19 21:45:13 +0700

    remove target from tracking

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-C:\Users\gamin\Documents\University\Adpro\Tutorial\Modul-6-Concurrency\hello\target\debug\deps\hello.d: src\main.rs
-
-C:\Users\gamin\Documents\University\Adpro\Tutorial\Modul-6-Concurrency\hello\target\debug\deps\hello.exe: src\main.rs
-
-src\main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/gamin/Documents/University/Adpro/Tutorial/Modul-6-Concurrency/hello/target/debug/hello: /mnt/c/Users/gamin/Documents/University/Adpro/Tutorial/Modul-6-Concurrency/hello/src/main.rs
